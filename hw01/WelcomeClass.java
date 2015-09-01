12a99c11efd16fd95efdb67a98226fea049de79a
billyopp6 <wpo213@lehigh.edu>
2015-09-01 20:27:22 +0000

    update WelcomeClass

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,18 @@
+//Billy Oppenheimer
+//hw01
+//Welcome Class Java Program
+
+public class WelcomeClass {
+    
+    public static void main(String[] args) {
+        System.out.println("  -----------");
+        System.out.println("  | WELCOME |");
+        System.out.println("  -----------");
+        System.out.println(" ^  ^  ^  ^  ^  ^");
+        System.out.println("/ \\/ \\/ \\/ \\/ \\/ \\ ");
+        System.out.println("<-W--P--0--2--1--3->");
+        System.out.println("\\ /\\ /\\ /\\ /\\ /\\ /");
+        System.out.println(" v  v  v  v  v  v");
+        System.out.println("This summer I interned in New York City and lived on the Upper West Side with four friends from high school.");
+    }
+}
